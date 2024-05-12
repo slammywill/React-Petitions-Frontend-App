6eb5e31b55b520f958594b3cb9343c4cbc801076
Sam Willems <swi126@uclive.ac.nz>
2024-05-13 01:08:42 +1200

    added petition filtering via search, category and max supporting cost

--- src/components/Petitions.tsx
+++ src/components/Petitions.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import axios from "axios";
 import CSS from "csstype";
 import PetitionsObject from "./PetitionsObject";
-import {Link, useNavigate, useParams} from "react-router-dom";
-import {Paper, AlertTitle, Alert } from "@mui/material";
+import {Paper, AlertTitle, Alert, FormControl, InputAdornment, Input, ToggleButtonGroup, ToggleButton, InputLabel } from "@mui/material";
+import {Unstable_NumberInput as NumberInput} from "@mui/base";
+import {Search} from "@mui/icons-material";
 
 const Petitions = () => {
     const [petitions, setPetitions] = React.useState<Array<Petition>>([])
+    const [filteredPetitions, setFilteredPetitions] = React.useState<Array<Petition>>([])
     const [errorFlag, setErrorFlag] = React.useState(false)
     const [errorMessage, setErrorMessage] = React.useState("")
+    const [searchQuery, setSearchQuery] = React.useState("")
+    const [selectedCategories, setSelectedCategories] = React.useState<Array<string>>([])
+    const [maxSupportingCost, setMaxSupportingCost] = React.useState(Number("NaN"))
 
     const getPetitions = () => {
         axios.get("http://localhost:4941/api/v1/petitions")
@@ -26,18 +31,89 @@ const Petitions = () => {
         getPetitions()
     }, [setPetitions])
 
-    const petition_rows = () => petitions.map((petition: Petition) => <PetitionsObject key={ petition.petitionId } petition={petition}/>)
+    React.useEffect(() => {
+        let filtered = petitions.filter((petition) => 
+            petition.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        if (selectedCategories.length > 0) {
+            filtered = filtered.filter((petition) => 
+                selectedCategories.includes(petition.categoryId.toString())
+            );
+        }
+        if (!isNaN(Number(maxSupportingCost))) {
+            filtered = filtered.filter((petition) =>
+                petition.supportingCost <= maxSupportingCost);
+        }
+
+        setFilteredPetitions(filtered);
+    }, [searchQuery, selectedCategories, maxSupportingCost, petitions]);
+
+    const petition_rows = () => filteredPetitions.map((petition: Petition) => 
+        <PetitionsObject key={ petition.petitionId } petition={petition}/>)
 
     const card: CSS.Properties = {
         padding: "10px",
         margin: "20px",
-        display: "block",
-        width: "fit-content"
+        display: "grid",
+        width: "fill"
     }
+
+    const handleCategorySelection = (
+        event: React.MouseEvent<HTMLElement>,
+        selected: string[],
+    ) => {
+        setSelectedCategories(selected);
+    };
+
+    const handleMaxSupportingCostChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        const value = event.target.value;
+        if (!isNaN(Number(value))) {
+            setMaxSupportingCost(Number(value));
+        }
+    };
 
     return (
         <Paper elevation={3} style={card} >
             <h1>Petition List</h1>
+            <div id="search-items" >
+                <Input
+                    id="search-bar"
+                    placeholder="Search"
+                    onChange={(val) => setSearchQuery(val.target.value)}
+                    startAdornment={
+                        <InputAdornment position="start">
+                            <Search /> 
+                        </InputAdornment>
+                    }
+                />
+                <div style={{display: "flex", justifyContent: "center"}}>
+                    <p style={{marginLeft: "10px", marginRight: "10px"}}>Supporting Cost &lt;= </p>
+                    <Input 
+                        placeholder="Type a number..."
+                        type="number"
+                        onChange={handleMaxSupportingCostChange}
+                    />
+                </div>
+                <ToggleButtonGroup size="small"
+                    onChange={handleCategorySelection}
+                    color="primary"
+                    value={selectedCategories}
+                    style={{margin:"10px"}}
+                >
+                    <ToggleButton value="1">Wildlife</ToggleButton>
+                    <ToggleButton value="2">Environmental Causes</ToggleButton>
+                    <ToggleButton value="3">Animal Rights</ToggleButton>
+                    <ToggleButton value="4">Education</ToggleButton>
+                    <ToggleButton value="5">Human Rights</ToggleButton>
+                    <ToggleButton value="6">Technology and Innovation</ToggleButton>
+                    <ToggleButton value="7">Arts and Culture</ToggleButton>
+                    <ToggleButton value="8">Community Development</ToggleButton>
+                    <ToggleButton value="9">Economic Empowerment</ToggleButton>
+                    <ToggleButton value="10">Science and Research</ToggleButton>
+                    <ToggleButton value="11">Sports and Recreation</ToggleButton>
+                </ToggleButtonGroup>
+            </div>
             <div style={{ display: "inline-block",  minWidth: "320px" }}>
                 {errorFlag?
                     <Alert severity = "error">
--- src/components/PetitionsObject.tsx
+++ src/components/PetitionsObject.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "../App.css";
+import defaultImage from "../resources/default_profile_image.png";
 import axios from "axios";
 import CSS from 'csstype';
 import { Card, Button, CardActions, CardContent, CardMedia, Dialog, Typography } from "@mui/material";
@@ -7,28 +9,57 @@ interface IPetitionProps {
     petition: Petition
 }
 
+
 const PetitionsObject = (props: IPetitionProps) => {
     const [petition] = React.useState<Petition>(props.petition)
     const [openDeleteDialog, setOpenDeleteDialog] = React.useState(false)
-    const [imageUrl, setImageUrl] = React.useState("");
+    const [petitionImageUrl, setPetitionImageUrl] = React.useState("");
+    const [ownerImageUrl, setOwnerImageUrl] = React.useState("");
 
     React.useEffect(() => {
         axios.get("http://localhost:4941/api/v1/petitions/" + petition.petitionId + "/image", {responseType: 'blob' })
             .then( response =>  {
                 const url = URL.createObjectURL(response.data);
-                setImageUrl(url);
+                setPetitionImageUrl(url);
             })
             .catch(error => {
                 console.error(error.toString());
             })
     }, [])
 
+    React.useEffect(() => {
+        axios.get("http://localhost:4941/api/v1/users/" + petition.ownerId + "/image", {responseType: 'blob' })
+            .then( response =>  {
+                const url = URL.createObjectURL(response.data);
+                setOwnerImageUrl(url);
+            })
+            .catch(error => {
+                console.log(error.toString());
+            })
+    }, [])
+
+    let categoryMap = new Map<number, string>([
+        [1, "Wildlife"],
+        [2, "Environmental Causes"],
+        [3, "Animal Rights"],
+        [4, "Health and Wellness"],
+        [5, "Education"],
+        [6, "Human Rights"],
+        [7, "Technoligy and Innovation"],
+        [8, "Arts and Culture"],
+        [9, "Community Development"],
+        [10, "Economic Empowerment"],
+        [11, "Science and Research"],
+        [12, "Sports and Recreation"]
+    ]);
+
     const petitionCardStyles: CSS.Properties = {
         display: "inline-block",
-        height: "350px",
+        height: "520px",
         width: "300px",
         margin: "10px",
-        padding: "0px"
+        padding: "0px",
+        alignItems: "start",
     }
 
     return (
@@ -36,15 +67,27 @@ const PetitionsObject = (props: IPetitionProps) => {
             <CardMedia
                 component="img"
                 height="200px"
-                width="200px"
+                width="300px"
                 sx={{objectFit:"cover"}}
-                src={imageUrl}
+                src={petitionImageUrl}
                 alt="Petition Image"
             />
             <CardContent>
-                <Typography variant="h4">
+                <Typography variant="h6">
                     {petition.title}
                 </Typography>
+                <div style={{display: "block"}}>
+                    <p>{petition.ownerFirstName} {petition.ownerLastName}</p>
+                    <img
+                        src={ownerImageUrl}
+                        alt=""
+                        id="profile-image"
+                        onError={(e) => {(e.target as HTMLImageElement).src = defaultImage}}
+                    />
+                    <p>Created: {petition.creationDate}</p>
+                    <p>Category: {categoryMap.get(petition.categoryId)}</p>
+                    <p>Minimum tier supporting cost: ${petition.supportingCost}.00</p>
+                </div>
             </CardContent>
         </Card>
     )
